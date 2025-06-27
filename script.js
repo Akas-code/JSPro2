9d6ec9cb6b0b9428a919c480f8d684bb7598344f
Akash <codesecoder@gmail.com>
2025-06-27 22:45:04 +0530

    1st

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = document.querySelector("#weight").value;
+  const result = document.querySelector("#result");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    result.innerHTML = `Please enter both height and weight ${height} ${weight}`;
+  }
+
+  result.innerHTML = `Your BMI is ${height}`;
+});
